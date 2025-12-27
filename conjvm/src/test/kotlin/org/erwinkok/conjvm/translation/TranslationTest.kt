82e26a18126e6f6e5d16ac492d988689098e03bf
Erwin Kok <erwin.kok@protonmail.com>
2025-12-27 23:25:24 +0100

    Update

--- conjvm/src/test/kotlin/org/erwinkok/conjvm/translation/TranslationTest.kt
+++ conjvm/src/test/kotlin/org/erwinkok/conjvm/translation/TranslationTest.kt
@@ -6,32 +6,35 @@ import org.erwinkok.conjvm.tac.TacTranslation
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.io.StringWriter
+import java.util.zip.GZIPInputStream
 
 class TranslationTest {
     @Test
     fun translationTest() {
         val classLoader = TranslationTest::class.java.classLoader
-        val inputStream = classLoader.getResourceAsStream("input.c")
+        val inputStream = classLoader.getResourceAsStream("input.c.gz")
         requireNotNull(inputStream)
+        GZIPInputStream(inputStream).use { inputGzip ->
+            val compilationUnit = Parser.parseStream(inputGzip)
 
-        val compilationUnit = Parser.parseStream(inputStream)
+            var translatedCompilationUnit = Translator.translateStatement(compilationUnit, ConvertForToWhileTranslation())
+            translatedCompilationUnit = Translator.translateStatement(translatedCompilationUnit, AssignmentTranslation())
 
-        var translatedCompilationUnit = Translator.translateStatement(compilationUnit, ConvertForToWhileTranslation())
-        translatedCompilationUnit = Translator.translateStatement(translatedCompilationUnit, AssignmentTranslation())
+            val translationVisitor = TacTranslation()
+            translationVisitor.translateStatement(translatedCompilationUnit)
 
-        val translationVisitor = TacTranslation()
-        translationVisitor.translateStatement(translatedCompilationUnit)
+            val referenceStream = classLoader.getResourceAsStream("reference.c.gz")
+            requireNotNull(referenceStream)
+            GZIPInputStream(referenceStream).use { gzip ->
+                val expected = String(gzip.readBytes())
 
-        val referenceStream = classLoader.getResourceAsStream("reference.c")
-        requireNotNull(referenceStream)
-        val reference = String(referenceStream.readAllBytes())
+                val writer = StringWriter()
+                val tacCodeWriter = TacCodeWriter(writer)
+                tacCodeWriter.printFunctions(translationVisitor.functions)
+                val actual = writer.toString()
 
-        val writer = StringWriter()
-        val tacCodeWriter = TacCodeWriter(writer)
-        tacCodeWriter.printFunctions(translationVisitor.functions)
-
-        val actual = writer.toString()
-
-        assertEquals(reference, actual)
+                assertEquals(expected, actual)
+            }
+        }
     }
 }
